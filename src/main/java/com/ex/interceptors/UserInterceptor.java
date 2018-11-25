1dab479578b585f95f9574be58cd3067fea9c215
alex <alex@qq.com>
2018-11-25 12:04:29 +0800

    modify interceptor

--- src/main/java/com/ex/interceptors/UserInterceptor.java
+++ src/main/java/com/ex/interceptors/UserInterceptor.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserInterceptor implements HandlerInterceptor {
 
     public boolean preHandle(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse, Object o) throws Exception {
-        return false;
+        return true;
     }
 
     public void postHandle(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
